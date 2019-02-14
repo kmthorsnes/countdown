1df854d0e9b7deead4fdccdf407a2e8909461c99
kalle <kmthorsnes@gmail.com>
2019-02-14 20:58:39 +0100

    refactor for last 10 numbers being red, not last 11

--- countdown.js
+++ countdown.js
@@ -6,11 +6,11 @@ const msPerCount = msForCountdown / initialValue;
 function countingDown(){
 	let faktor = (number + (initialValue / 2)) / initialValue;
     let msPerCurrentTimeout = msPerCount * faktor;
-    if (number <= 100 && number >= 11) {
-        console.log(number, faktor, msPerCurrentTimeout)
+    if (number <= 100 && number >= 10) {
+        console.log(number)
         setTimeout(countingDown, msPerCurrentTimeout); 
-   } else if (number <= 10 && number >= 0){
-        console.log('\x1b[31m%s\x1b[0m', number, faktor, msPerCurrentTimeout);
+   } else if (number <= 9 && number >= 0){
+        console.log('\x1b[31m%s\x1b[0m', number);
         setTimeout(countingDown, msPerCurrentTimeout); 
    }
    number = number - 1;
