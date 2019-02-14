9494e21cab46f059bfd4cb3ea5d63a477aad680c
kalle <kmthorsnes@gmail.com>
2019-02-14 21:33:16 +0100

    adds readme.md

--- countdown.js
+++ countdown.js
@@ -1,18 +1,18 @@
-const msForCountdown = process.argv[2] * 1000;
+const msForCountdown = process.argv[2] * 1000
 const initialValue = 100;
 let number = initialValue;
 const msPerCount = msForCountdown / initialValue;
 
-function countingDown() {
-  let faktor = (number + initialValue / 2) / initialValue;
-  let msPerCurrentTimeout = msPerCount * faktor;
-  if (number <= 100 && number >= 10) {
-    console.log(number);
-    setTimeout(countingDown, msPerCurrentTimeout);
-  } else if (number <= 9 && number >= 0) {
-    console.log("\x1b[31m%s\x1b[0m", number);
-    setTimeout(countingDown, msPerCurrentTimeout);
-  }
-  number = number - 1;
+function countingDown(){
+	let faktor = (number + (initialValue / 2)) / initialValue;
+    let msPerCurrentTimeout = msPerCount * faktor;
+    if (number <= 100 && number >= 10) {
+        console.log(number)
+        setTimeout(countingDown, msPerCurrentTimeout); 
+   } else if (number <= 9 && number >= 0){
+        console.log('\x1b[31m%s\x1b[0m', number);
+        setTimeout(countingDown, msPerCurrentTimeout); 
+   }
+   number = number - 1;
 }
 countingDown();
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Node JS Countdown
+
+---
+
+## A small javaScript that runs a countdown where the time between each countdown - 100 - 0 decrease with exponential value
+
+## Use
+
+- Download file
+- Navigate to file path folder in console
+- run the following command:
+
+```
+node countdown.js [seconds]
+```
+
+_Example_:
+
+To start a countdown for 35 seconds enter
+
+    node countdown.js 35
+
+## Technology
+
+- [NodeJS](https://nodejs.org/en/)
+- [JavaScript](https://developer.mozilla.org/bm/docs/Web/JavaScript)
